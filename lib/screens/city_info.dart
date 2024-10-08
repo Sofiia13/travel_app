8df6098b10f166545b5be3874f71388c93bde30b
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-08 23:53:58 +0300

    Added Dropdown button to choose category

--- lib/screens/city_info.dart
+++ lib/screens/city_info.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:travel_app/widgets/map.dart';
 
 class CityInfo extends StatefulWidget {
-  CityInfo({super.key, required this.cityName});
+  const CityInfo({super.key, required this.cityName});
 
   final String cityName;
 
@@ -17,26 +17,37 @@ class _CityInfoState extends State<CityInfo> {
   @override
   List<dynamic> _cityCoordinates = [];
 
-  double? xCor; // Store longitude
-  double? yCor; // Store latitude
+  String? _selectedCategory;
+
+  List<String> categories = [
+    'activity',
+    'commercial',
+    'catering',
+    'entertainment',
+    'healthcare',
+    'natural',
+    'tourism',
+    'religion',
+  ];
+
+  double? xCor;
+  double? yCor;
 
   void _getCityCoordinates(String name) async {
     final url = Uri.parse('https://api.api-ninjas.com/v1/city?name=$name');
     final response = await http.get(url, headers: {
-      'X-Api-Key':
-          'w1alNvn/gAEdtCWgy1cKgg==8aACMMX2MSFRlCmI', // Make sure to add your API key here
+      'X-Api-Key': 'w1alNvn/gAEdtCWgy1cKgg==8aACMMX2MSFRlCmI',
     });
 
     if (response.statusCode == 200) {
       final List<dynamic> listData = json.decode(response.body);
 
       setState(() {
-        _cityCoordinates = listData; // Store the entire response
+        _cityCoordinates = listData;
         if (_cityCoordinates.isNotEmpty) {
           xCor = _cityCoordinates[0]['longitude'];
           yCor = _cityCoordinates[0]['latitude'];
         } else {
-          // Handle the case where no data is returned
           xCor = null;
           yCor = null;
         }
@@ -56,6 +67,7 @@ class _CityInfoState extends State<CityInfo> {
   void initState() {
     super.initState();
     _getCityCoordinates(widget.cityName);
+    _selectedCategory = categories[0];
   }
 
   Widget build(BuildContext context) {
@@ -64,12 +76,42 @@ class _CityInfoState extends State<CityInfo> {
         title: Text(widget.cityName),
       ),
       body: (xCor != null && yCor != null)
-          ? MapWidget(xCor: xCor!, yCor: yCor!) // Use null assertion operator
+          ? Column(
+              children: [
+                Expanded(
+                  child: MapWidget(xCor: xCor!, yCor: yCor!),
+                ),
+                Expanded(
+                  child: DropdownButton(
+                    value: _selectedCategory,
+                    items: [
+                      for (final category in categories)
+                        DropdownMenuItem(
+                          value: category,
+                          child: Text(category),
+                        ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCategory = value!;
+                      });
+                    },
+                  ),
+                ),
+                // Expanded(
+                //   child: ListView.builder(
+                //     itemCount: categories.length,
+                //     itemBuilder: (context, index) {
+                //       return ;
+                //     },
+                //   ),
+                // ),
+              ],
+            )
           : Center(
               child: xCor == null && yCor == null
-                  ? CircularProgressIndicator() // Loading indicator
-                  : Text(
-                      'Coordinates not found. Please try again.'), // Fallback message
+                  ? const CircularProgressIndicator()
+                  : const Text('Coordinates not found. Please try again.'),
             ),
     );
   }
