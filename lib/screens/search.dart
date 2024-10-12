15c6bd6343c6a40108c769638e0a73f7c5d715cd
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-12 22:28:07 +0300

    Added listener for entered text

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final myController = TextEditingController();
+  String enteredText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // Listen to changes in the text field
+    myController.addListener(() {
+      setState(() {
+        enteredText = myController.text; // Update state when text changes
+      });
+    });
+  }
+
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Column(
       children: [
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextField(
-            decoration: InputDecoration(
+            controller: myController,
+            decoration: const InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(
                     Radius.circular(100),
@@ -20,6 +46,8 @@ class SearchScreen extends StatelessWidget {
                 hintText: 'Enter city you want to visit'),
           ),
         ),
+        const SizedBox(height: 10),
+        Text(enteredText),
       ],
     ));
   }
