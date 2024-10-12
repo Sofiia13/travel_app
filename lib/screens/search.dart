db0233e5d5affc177ca1de767c749ea5cee787ee
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-12 22:02:47 +0300

    Added TextField for search request

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -5,10 +5,22 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
-      ),
-    );
+    return const Scaffold(
+        body: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+                hintText: 'Enter city you want to visit'),
+          ),
+        ),
+      ],
+    ));
   }
 }
