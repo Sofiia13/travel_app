0fe849c7d907161eb9b07bc10657577ecdeb74c9
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-17 00:14:07 +0300

    Added function to login user

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_app/screens/logIn.dart';
 import 'package:travel_app/screens/signup.dart';
 import 'package:travel_app/screens/tabs.dart';
 
@@ -21,7 +22,8 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 58, 135, 183)),
       ),
       // home: const TabsScreen(),
-      home: const SignUpScreen(),
+      // home: const SignUpScreen(),
+      home: const LogInScreen(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -15,23 +15,19 @@ class _LogInScreenState extends State<LogInScreen> {
   String emailAddress = '';
   String password = '';
 
-  void createUserWithEmailAndPassword() async {
+  void signInWithEmailAndPassword() async {
     if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save(); // Save the form data (email, password)
+      _formKey.currentState!.save();
       try {
-        final credential =
-            await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: emailAddress,
-          password: password,
-        );
+        final credential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+                email: emailAddress, password: password);
       } on FirebaseAuthException catch (e) {
-        if (e.code == 'weak-password') {
-          print('The password provided is too weak.');
-        } else if (e.code == 'email-already-in-use') {
-          print('The account already exists for that email.');
+        if (e.code == 'user-not-found') {
+          print('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          print('Wrong password provided for that user.');
         }
-      } catch (e) {
-        print(e);
       }
     }
   }
@@ -56,7 +52,7 @@ class _LogInScreenState extends State<LogInScreen> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: AuthenticationForm(
-          authenticateUser: createUserWithEmailAndPassword,
+          authenticateUser: signInWithEmailAndPassword,
           formKey: _formKey,
           onEmailChanged: updateEmail,
           onPasswordChanged: updatePassword,
