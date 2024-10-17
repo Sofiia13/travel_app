624949462260db35b6f5b5d02f04708b5ef57142
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-17 14:16:21 +0300

    Small fix

--- lib/widgets/authentication_form.dart
+++ lib/widgets/authentication_form.dart
@@ -69,7 +69,7 @@ class AuthenticationForm extends StatelessWidget {
                 child: Text(buttonText),
               )
             ],
-          )
+          ),
         ],
       ),
     );
