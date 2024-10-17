89281db0906fa4c73cd289d0fdd1cf6e5735b8ed
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-17 14:16:54 +0300

    Added navigator to home screen and to signUp screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travel_app/screens/signup.dart';
+import 'package:travel_app/screens/tabs.dart';
 import 'package:travel_app/widgets/authentication_form.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -44,6 +47,22 @@ class _LogInScreenState extends State<LogInScreen> {
     });
   }
 
+  void _goToHomePage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const TabsScreen(),
+      ),
+    );
+  }
+
+  void _goToSignUp(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const SignUpScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // var _isSending = false;
@@ -51,13 +70,40 @@ class _LogInScreenState extends State<LogInScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: AuthenticationForm(
-          authenticateUser: signInWithEmailAndPassword,
-          formKey: _formKey,
-          onEmailChanged: updateEmail,
-          onPasswordChanged: updatePassword,
-          buttonText: 'Login',
-          goTo: () {},
+        child: Column(
+          children: [
+            AuthenticationForm(
+              authenticateUser: signInWithEmailAndPassword,
+              formKey: _formKey,
+              onEmailChanged: updateEmail,
+              onPasswordChanged: updatePassword,
+              buttonText: 'Login',
+              goTo: () {
+                _goToHomePage(context);
+              },
+            ),
+            const SizedBox(height: 20),
+            // Using RichText to display clickable Sign Up
+            RichText(
+              text: TextSpan(
+                text: "If you don't have an account, please ",
+                style: const TextStyle(color: Colors.black),
+                children: [
+                  TextSpan(
+                    text: 'Sign up',
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        _goToSignUp(context); // Navigate to SignUpScreen
+                      },
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
