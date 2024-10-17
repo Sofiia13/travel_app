43b6f0529d2417a845554a05079dfa8e74319ebf
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-17 14:54:26 +0300

    Added sign out icon button and navigator to login page

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/calendar.dart';
 import 'package:travel_app/screens/cities_list.dart';
+import 'package:travel_app/screens/logIn.dart';
 import 'package:travel_app/screens/search.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -21,6 +23,21 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _goToLoginPage(BuildContext context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (ctx) => const LogInScreen(),
+      ),
+    );
+  }
+
+  void signOut() async {
+    await FirebaseAuth.instance.signOut();
+    setState(() {
+      _goToLoginPage(context);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CitiesList();
@@ -36,6 +53,17 @@ class _TabsScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.exit_to_app,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              signOut();
+            },
+          )
+        ],
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
