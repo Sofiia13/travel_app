b03bfcd9f4c861355c354b9acdf32f8cf38f600f
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-21 20:44:10 +0300

    Receive info to create event

--- lib/widgets/filtered_places.dart
+++ lib/widgets/filtered_places.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/place_info.dart';
+import 'package:travel_app/widgets/google_calendar_service_factory.dart';
 
 class FilteredPlaces extends StatelessWidget {
-  const FilteredPlaces({
+  FilteredPlaces({
     super.key,
     required this.name,
-  });
+    required this.location,
+  }) : googleCalendarService = GoogleCalendarServiceFactory.create();
 
   final String name;
+  final String location;
+  final GoogleCalendarService googleCalendarService;
 
   void _selectPlace(BuildContext context, String placeName) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => PlaceInfoScreen(placeName: placeName),
+        builder: (ctx) => PlaceInfoScreen(
+          placeName: placeName,
+          location: location,
+          googleCalendarService: googleCalendarService,
+        ),
       ),
     );
   }
