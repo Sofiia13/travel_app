e8fb97ac1273b6247fa245cd1b44caa0d4508e8c
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-21 21:48:06 +0300

    Created form to create event

--- lib/screens/place_info.dart
+++ lib/screens/place_info.dart
@@ -58,7 +58,17 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
     _initializeCalendarService();
   }
 
-  TextEditingController _dateController = TextEditingController();
+  @override
+  void dispose() {
+    _dateController.dispose();
+    _startTimeController.dispose();
+    _endTimeController.dispose();
+    super.dispose();
+  }
+
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _startTimeController = TextEditingController();
+  final TextEditingController _endTimeController = TextEditingController();
 
   Future<void> _selectDate() async {
     DateTime? _picked = await showDatePicker(
@@ -73,6 +83,62 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
         _dateController.text = DateFormat('dd-MM-yyyy').format(_picked);
       });
     }
+  }
+
+  Future<void> _selectStartTime() async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      initialEntryMode: TimePickerEntryMode.dial,
+    );
+
+    if (pickedTime != null) {
+      setState(() {
+        _startTimeController.text = pickedTime.format(context);
+      });
+      print("Selected time: ${_startTimeController.text}");
+    }
+  }
+
+  Future<void> _selectEndTime() async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      initialEntryMode: TimePickerEntryMode.dial,
+    );
+
+    TimeOfDay? startTime = _getTimeOfDayFromString(_startTimeController.text);
+
+    if (pickedTime != null && startTime != null) {
+      // Compare total minutes of picked end time and start time
+      if (_timeToMinutes(pickedTime) > _timeToMinutes(startTime)) {
+        setState(() {
+          _endTimeController.text = pickedTime.format(context);
+        });
+        print("Selected end time: ${_endTimeController.text}");
+      } else {
+        // Show a message if the end time is not valid
+        Text('End time cannot be earlier than start time.');
+        print("End time cannot be earlier than start time.");
+        // You could also show a dialog or a SnackBar for user feedback
+      }
+    }
+  }
+
+  int _timeToMinutes(TimeOfDay time) {
+    return time.hour * 60 + time.minute;
+  }
+
+  TimeOfDay? _getTimeOfDayFromString(String timeString) {
+    if (timeString.isEmpty) return null;
+
+    final timeParts = timeString.split(':');
+    if (timeParts.length != 2) return null;
+
+    final hour = int.parse(timeParts[0]);
+    final minute = int.parse(timeParts[1]);
+
+    return TimeOfDay(hour: hour, minute: minute);
   }
 
   @override
@@ -105,11 +171,6 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
                         const SizedBox(
                           height: 10,
                         ),
-                        // DatePickerDialog(
-                        //   firstDate: DateTime.now().toUtc(),
-                        //   lastDate:
-                        //       DateTime.now().toUtc().add(Duration(days: 365)),
-                        // ),
                         TextField(
                           controller: _dateController,
                           decoration: const InputDecoration(
@@ -126,22 +187,34 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
                         const SizedBox(
                           height: 10,
                         ),
-                        const TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
+                        TextField(
+                          controller: _startTimeController,
+                          decoration: const InputDecoration(
                             border: OutlineInputBorder(),
                             labelText: 'Start time',
+                            filled: true,
+                            prefixIcon: Icon(Icons.timer_outlined),
                           ),
+                          readOnly: true,
+                          onTap: () {
+                            _selectStartTime();
+                          },
                         ),
                         const SizedBox(
                           height: 10,
                         ),
-                        const TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
+                        TextField(
+                          controller: _endTimeController,
+                          decoration: const InputDecoration(
                             border: OutlineInputBorder(),
-                            labelText: 'Duration',
+                            labelText: 'End time',
+                            filled: true,
+                            prefixIcon: Icon(Icons.timer_outlined),
                           ),
+                          readOnly: true,
+                          onTap: () {
+                            _selectEndTime();
+                          },
                         ),
                         const SizedBox(
                           height: 10,
