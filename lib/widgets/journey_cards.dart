1851d46db1786ed467f785fb9c17e9be20e02afc
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-25 22:50:46 +0300

    Created Cards widget for journeys

---
+++ lib/widgets/journey_cards.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/screens/tabs.dart';
+
+class JourneyCards extends StatelessWidget {
+  const JourneyCards({super.key, required this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    void _goToHomePage(BuildContext context) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const TabsScreen(),
+        ),
+      );
+    }
+
+    return InkWell(
+      onTap: () {
+        _goToHomePage(context);
+      },
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(name),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
