19dad9903abe3d6ed00ac6e4f18731b5316222a7
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-25 22:18:56 +0300

    Added import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/create_journey.dart';
 import 'package:travel_app/screens/logIn.dart';
