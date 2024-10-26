ac282342714f66d04632064773028b31d6b56b47
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-26 21:56:46 +0300

    Restructured these components

--- lib/screens/city_info.dart
+++ lib/screens/city_info.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
-import 'package:travel_app/widgets/filtered_places.dart';
 import 'package:travel_app/widgets/map.dart';
+import 'package:travel_app/widgets/places_listView.dart';
 
 class CityInfo extends StatefulWidget {
-  const CityInfo({super.key, required this.cityName});
+  const CityInfo({
+    super.key,
+    required this.cityName,
+  });
 
   final String cityName;
 
@@ -15,18 +17,9 @@ class CityInfo extends StatefulWidget {
 }
 
 class _CityInfoState extends State<CityInfo> {
-  List<dynamic> _cityCoordinates = [];
-  List<dynamic> _places = [];
-  List<dynamic> _placeName = [];
-  List<dynamic> _placeLocation = [];
-  List<dynamic> _placeId = [];
-
-  int limit = 10;
   double? xCor;
   double? yCor;
   bool _isLoading = true;
-
-  final String apiKey = '731dfd7dfb0d4ebb99295e0cfe811177';
   String? selectedCategory;
 
   List<String> categories = [
@@ -47,66 +40,30 @@ class _CityInfoState extends State<CityInfo> {
     _getCityCoordinates(widget.cityName);
   }
 
-  void _getCityCoordinates(String name) async {
+  Future<void> _getCityCoordinates(String name) async {
     final url = Uri.parse(
         'http://api.openweathermap.org/geo/1.0/direct?q=$name&limit=1&appid=c84bc983237ae8f3e1cf6daa568aab15');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final List<dynamic> listData = json.decode(response.body);
-
-      setState(() {
-        _cityCoordinates = listData;
-        if (_cityCoordinates.isNotEmpty) {
-          xCor = _cityCoordinates[0]['lon'];
-          yCor = _cityCoordinates[0]['lat'];
+      if (listData.isNotEmpty) {
+        setState(() {
+          xCor = listData[0]['lon'];
+          yCor = listData[0]['lat'];
           selectedCategory = categories[0];
-          getPlacesByCategory(selectedCategory, xCor, yCor, 10);
-        } else {
-          xCor = null;
-          yCor = null;
-        }
-      });
+          _isLoading = false;
+        });
+      }
     } else {
       setState(() {
-        xCor = null;
-        yCor = null;
         _isLoading = false;
       });
       print('Failed to load city coordinates: ${response.statusCode}');
     }
   }
 
-  void getPlacesByCategory(selectedCategory, xCor, yCor, limit) async {
-    final url = Uri.parse(
-        'https://api.geoapify.com/v2/places?categories=$selectedCategory&filter=circle:${xCor},${yCor},2000&&limit=10&apiKey=$apiKey');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> listData = json.decode(response.body);
-      setState(() {
-        _places = listData['features'];
-
-        _placeName = _places
-            .map((place) => place['properties']['address_line1'])
-            .toList();
-        _placeLocation = _places
-            .map((place) => place['properties']['address_line2'])
-            .toList();
-        _placeId = _places
-            .map(
-                (place) => place['properties']['datasource']['raw']['wikidata'])
-            .toList();
-        _isLoading = false;
-      });
-    } else {
-      print('Failed to load places: ${response.statusCode}');
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -128,41 +85,30 @@ class _CityInfoState extends State<CityInfo> {
                       padding: const EdgeInsets.symmetric(horizontal: 8),
                       child: DropdownButton<String>(
                         value: selectedCategory,
-                        items: categories
-                            .map((category) => DropdownMenuItem(
-                                  value: category,
-                                  child: Text(category),
-                                ))
-                            .toList(),
+                        items: categories.map((category) {
+                          return DropdownMenuItem(
+                            value: category,
+                            child: Text(category),
+                          );
+                        }).toList(),
                         onChanged: (value) {
                           setState(() {
                             selectedCategory = value!;
-                            _isLoading = true;
-                            getPlacesByCategory(
-                                selectedCategory, xCor, yCor, 20);
                           });
                         },
                       ),
                     ),
                     Expanded(
-                      child: ListView.builder(
-                        itemCount: _placeName.length,
-                        itemBuilder: (context, index) {
-                          final placeId =
-                              _placeId.isNotEmpty ? _placeId[index] : null;
-                          return FilteredPlaces(
-                            name: _placeName[index],
-                            location: _placeLocation[index],
-                            placeId: placeId ?? '',
-                          );
-                        },
+                      child: PlacesListview(
+                        selectedCategory: selectedCategory,
+                        xCor: xCor!,
+                        yCor: yCor!,
                       ),
                     ),
                   ],
                 )
               : const Center(
-                  child: Text('Coordinates not found. Please try again.'),
-                ),
+                  child: Text('Coordinates not found. Please try again.')),
     );
   }
 }
---
+++ lib/widgets/places_listView.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:travel_app/widgets/filtered_places.dart';
+
+class PlacesListview extends StatefulWidget {
+  const PlacesListview({
+    super.key,
+    required this.selectedCategory,
+    required this.xCor,
+    required this.yCor,
+  });
+
+  final String? selectedCategory;
+  final double xCor;
+  final double yCor;
+
+  @override
+  State<PlacesListview> createState() => _PlacesListviewState();
+}
+
+class _PlacesListviewState extends State<PlacesListview> {
+  List<dynamic> _places = [];
+  bool _isLoading = true;
+
+  final String apiKey = '731dfd7dfb0d4ebb99295e0cfe811177';
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.selectedCategory != null) {
+      getPlacesByCategory(
+          widget.selectedCategory!, widget.xCor, widget.yCor, 10);
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant PlacesListview oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.selectedCategory != widget.selectedCategory) {
+      getPlacesByCategory(
+          widget.selectedCategory!, widget.xCor, widget.yCor, 10);
+    }
+  }
+
+  void getPlacesByCategory(
+      String selectedCategory, double xCor, double yCor, int limit) async {
+    final url = Uri.parse(
+        'https://api.geoapify.com/v2/places?categories=$selectedCategory&filter=circle:$xCor,$yCor,2000&limit=$limit&apiKey=$apiKey');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> listData = json.decode(response.body);
+      setState(() {
+        _places = listData['features'];
+        _isLoading = false;
+      });
+    } else {
+      print('Failed to load places: ${response.statusCode}');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return ListView.builder(
+      itemCount: _places.length,
+      itemBuilder: (context, index) {
+        final place = _places[index]['properties'];
+        return FilteredPlaces(
+          name: place['address_line1'] ?? 'Unknown',
+          location: place['address_line2'] ?? 'Unknown',
+          placeId: place['datasource']['raw']['wikidata'] ?? '',
+        );
+      },
+    );
+  }
+}
