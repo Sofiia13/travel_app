9a8ecec38d9bd0d2fa9a87377a0cf6939b8b3dfd
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-10-28 23:32:40 +0200

    Added map on this scrren

--- lib/screens/place_info.dart
+++ lib/screens/place_info.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:googleapis/calendar/v3.dart';
-import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:travel_app/widgets/add_place_to_favorites.dart';
 import 'package:travel_app/widgets/create_calendar_event_dialog.dart';
 import 'package:travel_app/widgets/google_calendar_service_factory.dart';
 import 'package:travel_app/widgets/place_photo.dart';
+import 'package:travel_app/widgets/show_map.dart';
+import 'package:geocoding/geocoding.dart';
 
 class PlaceInfoScreen extends StatefulWidget {
   const PlaceInfoScreen({
@@ -28,11 +29,14 @@ class PlaceInfoScreen extends StatefulWidget {
 
 class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
   GoogleCalendarService? _calendarService;
+  LatLng? _coordinates;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _initializeCalendarService();
+    _getCoordinatesFromAddress(widget.placeLocation);
   }
 
   void _initializeCalendarService() async {
@@ -50,6 +54,30 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
     }
   }
 
+  void _getCoordinatesFromAddress(String address) async {
+    try {
+      // Use the geocoding package to get coordinates from the address
+      List<Location> locations = await locationFromAddress(address);
+      if (locations.isNotEmpty) {
+        setState(() {
+          _coordinates =
+              LatLng(locations.first.latitude, locations.first.longitude);
+          _isLoading = false; // Update loading state
+        });
+      } else {
+        print("No coordinates found for the address: $address");
+        setState(() {
+          _isLoading = false; // Update loading state
+        });
+      }
+    } catch (e) {
+      print("Error occurred while fetching coordinates: $e");
+      setState(() {
+        _isLoading = false; // Update loading state
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,34 +85,44 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
         title: Text(widget.placeName),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PlacePhoto(placeId: widget.placeId),
-            Text('Some place info'),
-            Text('Bla bla bla'),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  AddPlaceToFavorites(
-                    journeyId: widget.journeyId,
-                    placeName: widget.placeName,
-                    placeLocation: widget.placeLocation,
-                    placeId: widget.placeId,
-                  ),
-                  CreateEventDialog(
-                    name: widget.placeName,
-                    location: widget.placeLocation,
-                    googleCalendarService: widget.googleCalendarService,
-                    journeyId: widget.journeyId,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: _isLoading
+            ? CircularProgressIndicator() // Show loading indicator while fetching coordinates
+            : _coordinates != null
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PlacePhoto(placeId: widget.placeId),
+                      Text('Some place info'),
+                      Text('Bla bla bla'),
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            ShowMap(
+                              xCor: _coordinates!.longitude,
+                              yCor: _coordinates!.latitude,
+                            ),
+                            AddPlaceToFavorites(
+                              journeyId: widget.journeyId,
+                              placeName: widget.placeName,
+                              placeLocation: widget.placeLocation,
+                              placeId: widget.placeId,
+                            ),
+                            CreateEventDialog(
+                              name: widget.placeName,
+                              location: widget.placeLocation,
+                              googleCalendarService:
+                                  widget.googleCalendarService,
+                              journeyId: widget.journeyId,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                : Text(
+                    'Unable to retrieve coordinates for $widget.placeLocation'), // Message when coordinates are not found
       ),
     );
   }
