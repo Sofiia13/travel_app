5db9a3df1bb0254a628a1ba4601e99a4b176b4b1
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-11-01 21:01:39 +0200

    Added design to createJourneyScreen

--- lib/widgets/journey_cards.dart
+++ lib/widgets/journey_cards.dart
@@ -28,14 +28,59 @@ class JourneyCards extends StatelessWidget {
         _goToHomePage(context);
       },
       child: Card(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(name),
-            ],
+        color: Color.fromARGB(255, 221, 239, 255),
+        elevation: 2,
+        shadowColor: Colors.grey.withOpacity(0.2),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 187, 230, 250), // Light blue
+                Color.fromARGB(255, 123, 192, 248), // Slightly darker shade
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.travel_explore,
+                      color: Colors.blue[700],
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Start your adventure!",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
