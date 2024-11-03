90f0a48d4c5f4c38ab3151cad43036afed180ef4
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-11-03 23:35:47 +0200

    Added new dynamic data to the screen(description, location, website, phone)

--- lib/screens/place_info.dart
+++ lib/screens/place_info.dart
@@ -6,22 +6,32 @@ import 'package:travel_app/widgets/google_calendar_service_factory.dart';
 import 'package:travel_app/widgets/place_photo.dart';
 import 'package:travel_app/widgets/show_map.dart';
 import 'package:geocoding/geocoding.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:travel_app/widgets/text_with_place_info.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PlaceInfoScreen extends StatefulWidget {
   const PlaceInfoScreen({
     super.key,
     required this.placeName,
     required this.placeLocation,
+    required this.wikiPlaceId,
     required this.placeId,
+    this.fee,
+    this.phone,
+    this.website,
+    this.openingHours,
     required this.googleCalendarService,
     required this.journeyId,
   });
 
   final String placeName;
   final String placeLocation;
+  final String wikiPlaceId;
   final String placeId;
+  final String? fee;
+  final String? phone;
+  final String? website;
+  final String? openingHours;
   final GoogleCalendarService googleCalendarService;
   final String journeyId;
 
@@ -32,14 +42,12 @@ class PlaceInfoScreen extends StatefulWidget {
 class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
   LatLng? _coordinates;
   bool _isLoading = true;
-  String description = '';
 
   @override
   void initState() {
     super.initState();
     _initializeCalendarService();
     _getCoordinatesFromAddress(widget.placeLocation);
-    _getPlaceInfoFromWiki(widget.placeId);
   }
 
   void _initializeCalendarService() async {
@@ -47,7 +55,6 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
         await widget.googleCalendarService.signInAndGetCalendarApi();
 
     if (calendarApi != null) {
-      // Successfully authenticated
       print("Successfully authenticated and obtained Calendar API");
       setState(() {});
     } else {
@@ -78,59 +85,6 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
     }
   }
 
-  String capitalizeFirstLetter(String text) {
-    if (text.isEmpty) {
-      return text;
-    }
-    return text[0].toUpperCase() + text.substring(1);
-  }
-
-  Future<void> _getPlaceInfoFromWiki(String id) async {
-    final url = Uri.parse(
-        'https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items/$id');
-    final headers = {
-      'Accept': 'application/json',
-      'Authorization':
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmYmI1ZDk1ZTJiYTg3NDBmYmYwOWZiMTlkZWRjYjkwNyIsImp0aSI6IjE3OGZjYjUwYWFlNjc5NWI2OWE5ODQzOTBmZmVmZTg1NTQzZDc5OGM2ZDkxZmVjZTg1OGFlMzMzMjQ1ODYyZWYyYTMwZDRlNDQwMGMzMWMzIiwiaWF0IjoxNzI5NjA1NjExLjM5MTY3LCJuYmYiOjE3Mjk2MDU2MTEuMzkxNjc2LCJleHAiOjMzMjg2NTE0NDExLjM4NDQzNCwic3ViIjoiNzY3NzUzMDAiLCJpc3MiOiJodHRwczovL21ldGEud2lraW1lZGlhLm9yZyIsInJhdGVsaW1pdCI6eyJyZXF1ZXN0c19wZXJfdW5pdCI6NTAwMCwidW5pdCI6IkhPVVIifSwic2NvcGVzIjpbImJhc2ljIiwiaGlnaHZvbHVtZSIsInZpZXdteXdhdGNobGlzdCIsImVkaXRteXdhdGNobGlzdCIsImNoZWNrdXNlci10ZW1wb3JhcnktYWNjb3VudCJdfQ.lAZZ_mYuU2UqIc4ZyU6iZ6-coT1U6ltziDJk_9znO0DHG5EMrbYlrtlKPhM7Ft0KL7fnu-qJ3Hkuw3YIjgG_VEbmUJtIr350w8Qp0zIpc4Gx2FB6od18UMnHZE21fHBQDWnCR0EHXnj31HPDLM0CTGYhAP77fOOpE2ADJHpVEQU3FqjTTTE37sev-_BoslGg_fCDARc1Vsq9rZ7rjNGP3liGsTjzT4LZE2BiZWaTY_qHuHybomNncnpYe0VFhsSHu8QfjkF89TweEszBGrDKNS3uHXc6T8MBZa3RTCrCmWGe2U6fd9pv3jWacxbc-nfCgEo-R4Ud96mNygSAoL7yKy61KJMLSfR_QwMOa9r0MZDSqy_3UuOE8EuvwddFzRma3esAni6f8h25GLPv6_pyh3NsF8u59nC2AI3jOIdP2US_kvz8L80YlUSvGSedJk5wbXAFwd5ZlJA9Lv7Jlh67oA-0tEA7cuivtSsG9Kjyv9ifE1v-0ik8j80cqSBkspYVUnlMP9Fk9BnFyHyIymEko6c-COjD1icy7ci8DXWmRboS63MYZpyymI2ux804NakH3Jy3_hPagAVm99aZd4Ykis8POk3EQI-LfiYKF7Pw5GJpeTdI_P8YtaoJvBSzvm0VPxJOmH_QcCz51B4xV7ZzJp3mJN-F4FIzsL7dkRfkqD8', // Customize this as necessary
-    };
-
-    final response = await http.get(url, headers: headers);
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> jsonData = json.decode(response.body);
-
-      if (jsonData.containsKey('descriptions') &&
-          jsonData['descriptions'] is Map) {
-        final Map<String, dynamic> descriptions = jsonData['descriptions'];
-
-        if (descriptions.containsKey('en')) {
-          setState(() {
-            description = capitalizeFirstLetter(descriptions['en']);
-            _isLoading = false;
-          });
-        } else if (descriptions.containsKey('fr')) {
-          setState(() {
-            description = capitalizeFirstLetter(descriptions['fr']);
-            _isLoading = false;
-          });
-        } else {
-          print('No English description found for the given place.');
-          setState(() {
-            description = 'Description not available.';
-            _isLoading = false;
-          });
-        }
-      } else {
-        setState(() {
-          _isLoading = false;
-        });
-      }
-      print('Failed to load place description: ${response.statusCode}');
-    } else {
-      print('Can not connect to WikiAPI');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -154,16 +108,79 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
                         clipBehavior: Clip.hardEdge,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(15),
-                          child: PlacePhoto(placeId: widget.placeId),
+                          child: PlacePhoto(placeId: widget.wikiPlaceId),
                         ),
                       ),
-                      description.isNotEmpty
-                          ? Text(
-                              description,
-                              style: TextStyle(fontSize: 16),
-                            )
-                          : Container(),
-                      Text('Bla bla bla'),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 5,
+                        ),
+                        child: TextWithPlaceInfo(
+                          placeId: widget.placeId,
+                          wikiPlaceId: widget.wikiPlaceId,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        child: DefaultTextStyle(
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Location: ${widget.placeLocation}'),
+                              SizedBox(height: 3),
+                              if (widget.phone != null)
+                                Text('Phone: ${widget.phone}'),
+                              SizedBox(height: 3),
+                              if (widget.website != null)
+                                InkWell(
+                                  child: Text.rich(
+                                    TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Website: ',
+                                          style: TextStyle(color: Colors.black),
+                                        ),
+                                        TextSpan(
+                                          text: widget.website,
+                                          style: TextStyle(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
+                                            decoration:
+                                                TextDecoration.underline,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  onTap: () async {
+                                    String website = widget.website!;
+                                    if (!website.startsWith('http://') &&
+                                        !website.startsWith('https://')) {
+                                      website = 'https://$website';
+                                    }
+                                    final Uri websiteUri = Uri.parse(website);
+                                    if (await canLaunchUrl(websiteUri)) {
+                                      await launchUrl(websiteUri,
+                                          mode: LaunchMode.externalApplication);
+                                    } else {
+                                      print("Could not launch $website");
+                                    }
+                                  },
+                                ),
+                            ],
+                          ),
+                        ),
+                      ),
                       Padding(
                         padding: const EdgeInsets.all(15),
                         child: Row(
@@ -178,6 +195,7 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
                               journeyId: widget.journeyId,
                               placeName: widget.placeName,
                               placeLocation: widget.placeLocation,
+                              wikiPlaceId: widget.wikiPlaceId,
                               placeId: widget.placeId,
                             ),
                             // SizedBox(width: 5),
@@ -194,7 +212,8 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
                     ],
                   )
                 : Text(
-                    'Unable to retrieve coordinates for $widget.placeLocation'),
+                    'Unable to retrieve coordinates for $widget.placeLocation',
+                  ),
       ),
     );
   }
